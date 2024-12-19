b97f6caab1756500fde062547e8d3c4f11f2ad1c
Yin <1324236706@qq.com>
2024-12-19 14:31:30 +0800

    Index更新
    从70多m的动态背景改为静态背景加动效

--- js/titlemusic.js
+++ js/titlemusic.js
@@ -1,27 +1,27 @@
 const music = document.getElementById('music');
-const img=document.getElementsByClassName("right");
-const video=document.querySelector('.productVideo');
-const videoPanel=document.querySelector('.videoPanel');
-const bg=document.querySelector('.main_body_bg');
+const image1 = document.getElementsByClassName("right");
+const video = document.querySelector('.productVideo');
+const videoPanel = document.querySelector('.videoPanel');
+const bg = document.querySelector('.main_body_bg');
 let isPlaying = false;
 function playMusic() {
     if (!isPlaying) {
         music.play();
-        img.src="images/musicPlay.png";
+        image1.src = "images/musicPlay.png";
         isPlaying = true;
     }
 }
 function pauseMusic() {
     if (isPlaying) {
         music.pause();
-        img.src="images/musicMute.png";
+        image1.src = "images/musicMute.png";
         isPlaying = false;
     }
 }
-function changeState(){
-    if(isPlaying){
+function changeState() {
+    if (isPlaying) {
         pauseMusic();
-    }else{
+    } else {
         playMusic();
     }
 }
@@ -32,16 +32,16 @@ document.addEventListener('visibilitychange', () => {
         playMusic();
     }
 });
-function playVideo(){
+function playVideo() {
     music.pause();
-    video.className='productVideo-active';
+    video.className = 'productVideo-active';
     video.play();
-    videoPanel.className='videoPanel-active'
+    videoPanel.className = 'videoPanel-active'
     console.log('点击了按钮');
 }
-function exitVideo(){
+function exitVideo() {
     playMusic();
-    video.className='productVideo';
+    video.className = 'productVideo';
     video.pause();
-    videoPanel.className='videoPanel';
+    videoPanel.className = 'videoPanel';
 }
